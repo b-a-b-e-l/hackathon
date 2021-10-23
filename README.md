52a9a859f484d9e598703b2a3beb1966f9ea2277
Maria Belen Bednarski <72574650+b-a-b-e-l@users.noreply.github.com>
2021-10-23 17:06:15 +0200

    Created repository

--- README.md
+++ README.md
@@ -1,1 +1,26 @@
-# hackathon
+# hackathon-2021.vanilla-js.base-example
+
+This repository was created to be used during the [SheSharp Hackathon 2021](https://www.shesharp.co/2021-hackathon/) as a boilerplate for one of the challenges proposed.
+
+## Requirements
+
+To be able to develop a Vanilla website you will need:
+
+1. A github account and repository
+2. [VS Code](https://code.visualstudio.com/download) installed on your machine
+3. [Live Server](https://marketplace.visualstudio.com/items?itemName=ritwickdey.LiveServer) extension on VS Code
+4. With Live Server installed you can start a dev server follow the instructions on the install page
+5. Now you are ready to develop your site!
+
+## How do I set up github pages?
+
+- On your github repo go to the settings tab
+- In the menu at the left select the Pages tab
+- At **Source** select the branch: `main`
+- Keep the folder as: "/ (root)"
+
+Now you should see a green popup with your link
+
+## How can I update my github pages site?
+
+Because you have set up the GitHub pages it should be updated when you update your selected branch
---
+++ source/index.js
@@ -0,0 +1,29 @@
+const bubble = document.getElementById("bubble")
+const cat = document.getElementById("cat")
+const gifs = [
+            "source/images/gati-100.gif", "source/images/gati-yass.gif", 
+            "source/images/gati-great.gif", "source/images/gati-win.gif", 
+            "source/images/gati-awe.gif", "source/images/gati-wow.gif"]
+
+window.addEventListener("load", function() {
+    const loader = this.document.getElementById("loader")
+    loader.className += " hidden"
+})
+
+fetch("https://opensheet.vercel.app/124kEA4HsZhu5nAV9xQ1iq-0ob9GqFwHU72JYHDSlY2o/shoutout")
+.then(res => res.json())
+.then(data => { 
+    console.log(data)
+    const randomShout = data[Math.floor(Math.random()*data.length)];
+    const randomCat = gifs[Math.floor(Math.random()*gifs.length)]
+    console.log(randomShout)
+    bubble.style.backgroundColor = `${randomShout.backgroundcolor}`
+    //bubble.style.borderColor = `${randomShout.color}`
+    bubble.style.color = `${randomShout.color}`
+    bubble.innerHTML = `
+    <div>${randomShout.text}</div>
+    `
+    cat.innerHTML = `
+    <img src="${randomCat}" class="hola" alt="animated cat illustration celebrating"/>
+    `
+})
